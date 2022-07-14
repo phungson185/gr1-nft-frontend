d13f611f866103997815964aa3337c7d890f60bf
phungson185 <phungson185@gmail.com>
2022-07-14 09:06:34 +0700

    fix: bug network

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import profile from './profileSlice';
 import system from './systemSlice';
+import network from './networkSlice';
 
 export const store = configureStore({
   reducer: {
     profile,
     system,
+    network,
   },
 });
 
